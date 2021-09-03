4dbebf7718d94c1ed317a9196dfc9710e2c4af8a
Jac Clarke <clarkejac@ecs.vuw.ac.nz>
2021-09-03 17:40:50 +1200

    Added temporary image for key. Fixed commit author

--- src/nz/ac/vuw/ecs/swen225/gp21/render/BoardRender.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/render/BoardRender.java
@@ -8,12 +8,15 @@ import java.awt.*;
 public class BoardRender {
 	private JPanel boardPanel;
 	private ImageIcon chapImage = new ImageIcon(this.getClass().getResource("images/Chap.png"));
+	private ImageIcon keyImage = new ImageIcon(this.getClass().getResource("images/card.png"));
 	private JLabel chapLabel = new JLabel();
+	private JLabel keyLabel = new JLabel();
 	
 	/**
 	 * Packages board objects
 	 */
 	public BoardRender() {
+		keyLabel.setIcon(keyImage);
 		chapLabel.setIcon(chapImage);
 		boardPanel.add(chapLabel);
 	}
