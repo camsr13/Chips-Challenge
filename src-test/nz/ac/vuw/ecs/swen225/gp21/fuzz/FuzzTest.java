fca9172bd1b200b46000db3f0e38b4a3683870a2
Rhys Macdonald <macdonrhys@ecs.vuw.ac.nz>
2021-09-06 18:10:21 +1200

    Added random move generator

--- src-test/nz/ac/vuw/ecs/swen225/gp21/fuzz/FuzzTest.java
+++ src-test/nz/ac/vuw/ecs/swen225/gp21/fuzz/FuzzTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.awt.event.MouseAdapter;
+import java.util.Random;
+import java.util.concurrent.*;
 
 import static org.mockito.Mockito.mock;
 
@@ -11,18 +13,29 @@ import static org.mockito.Mockito.mock;
  * @author Rhys Macdonald - 300516792
  */
 public class FuzzTest {
-    private MouseAdapter north;
-    private MouseAdapter east;
-    private MouseAdapter south;
-    private MouseAdapter west;
+    // Placeholders
+    private final MouseAdapter north = mock(MouseAdapter.class);
+    private final MouseAdapter east = mock(MouseAdapter.class);
+    private final MouseAdapter south = mock(MouseAdapter.class);
+    private final MouseAdapter west = mock(MouseAdapter.class);
+    private final MouseAdapter[] directions = new MouseAdapter[]{north, east, south, west};
+
+    private final int MOVES = 100;
+    private final int TIMEOUT = 30;
 
     @BeforeEach
     void setUp() {
-        // Placeholders
-        north = mock(MouseAdapter.class);
-        east = mock(MouseAdapter.class);
-        south = mock(MouseAdapter.class);
-        west = mock(MouseAdapter.class);
+
+
+
+    }
+
+    private void performRandomMoves() {
+        Random random = new Random();
+        for (int i = 0; i < MOVES; i++) {
+            MouseAdapter direction = directions[random.nextInt(directions.length)];
+            direction.mouseClicked(null);
+        }
     }
 
     /**
@@ -30,6 +43,18 @@ public class FuzzTest {
      */
     @Test
     void test1() {
+        // Load in level 1
+
+
+
+        final ExecutorService es = Executors.newSingleThreadExecutor();
+        Future<?> future = es.submit(this::performRandomMoves);
+        try {
+            future.get(TIMEOUT, TimeUnit.SECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            e.printStackTrace();
+        }
+        es.shutdownNow();
 
     }
 
