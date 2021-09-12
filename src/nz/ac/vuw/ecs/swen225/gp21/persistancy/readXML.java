8de0e910e6f253c454045c0b40b64359e11a473d
Rhys Hanrahan <rhys.c.hanrahan@gmail.com>
2021-09-12 14:02:20 +1200

    Added the creation of tiles for the tileMap in readXML Changed the return type of readXML from Tile[][] to void and call Game.setupGame at the end instead Commented out some of the code that checks for the tile type to prevent creating not creating any tiles Fixed the file path to testMap.xml when trying to read a file Added a colour attributeValue to door and key tiles in the xml file Added new method getColour in readXML to get the string colour of a key or door tile and return a corresponding Game.KeyColour

--- src/nz/ac/vuw/ecs/swen225/gp21/persistancy/readXML.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/persistancy/readXML.java
@@ -1,9 +1,6 @@
 package nz.ac.vuw.ecs.swen225.gp21.persistancy;
 
-import nz.ac.vuw.ecs.swen225.gp21.domain.Location;
-import nz.ac.vuw.ecs.swen225.gp21.domain.Player;
-import nz.ac.vuw.ecs.swen225.gp21.domain.Tile;
-
+import nz.ac.vuw.ecs.swen225.gp21.domain.*;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
@@ -11,6 +8,7 @@ import org.jdom2.input.SAXBuilder;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
@@ -29,60 +27,69 @@ public class readXML {
     private Tile[][] tilemap = new Tile[5][13];
     private Player player;
 
-    //Read xml file and return tilemap
-    public Tile[][] readXMLFile(/**String fileName**/) throws JDOMException, IOException {
+    //Read xml file and call setupGame
+    public void readXMLFile(/**String fileName**/) throws JDOMException, IOException {
         //Variables to hold the position where the file is up to
         Element rowElement = null;
         Element tileElement = null;
-        Element map = ((Document) (new SAXBuilder()).build(new File("textMap.xml"/**fileName**/))).getRootElement();
+        Element map = ((Document) (new SAXBuilder()).build(new File("src/nz/ac/vuw/ecs/swen225/gp21/persistancy/testMap.xml" /**fileName**/))).getRootElement();
 
         //Get player info (location)
         List<Element> playerInfo = map.getChildren("player");
-        player = new Player(new Location(Integer.parseInt(playerInfo.get(0).getText()), Integer.parseInt(playerInfo.get(1).getText())));
+        player = new Player(new Location(4, 3));
 
         //Loop through the file and goes through all the tileRows
         List<Element> rowsList = map.getChildren("tileRow");
         for(int i = 0; i < rowsList.size(); i++){
             rowElement = (Element) rowsList.get(i);
-            Iterator<Element> tutorialsIterator = rowElement.getChildren("tile").iterator();
+            Iterator<Element> Iterator = rowElement.getChildren("tile").iterator();
 
             //Loops through all the tiles within each tileRow
             int count = 0;
-            while(tutorialsIterator.hasNext()){
-                tileElement = (Element) tutorialsIterator.next();
+            while(Iterator.hasNext()){
+                tileElement = (Element) Iterator.next();
 
                 //check for what the tile type is then add it to the tile map
                 if(tileElement.getText().equals("free")){
-                    //tilemap[i][count] = new freeTile(new Location(count, i), true);
+                    tilemap[i][count] = new FreeTile(new Location(count, i));
                 }else if(tileElement.getText().equals("wall")){
-                    //tilemap[i][count] = new wallTile(new Location(count, i), false);
+                    tilemap[i][count] = new WallTile(new Location(count, i));
                 }else if(tileElement.getText().equals("door")){
-                    //tilemap[i][count] = new doorTile(new Location(count, i), false, colour/id);
+                    tilemap[i][count] = new LockTile(new Location(count, i), getColour(tileElement.getAttributeValue("colour")));
                 }else if(tileElement.getText().equals("key")){
-                    //tilemap[i][count] = new keyTile(new Location(count, i), true, colour/id);
-                }else if(tileElement.getText().equals("treasure")){
-                    //tilemap[i][count] = new treasureTile(new Location(count, i), true);
+                    tilemap[i][count] = new KeyTile(new Location(count, i), getColour(tileElement.getAttributeValue("colour")));
+                }/**else if(tileElement.getText().equals("treasure")){
+                    //tilemap[i][count] = new treasureTile(new Location(count, i));
                 }else if(tileElement.getText().equals("info")){
-                    //tilemap[i][count] = new infoTile(new Location(count, i), true, info);
+                    //tilemap[i][count] = new infoTile(new Location(count, i), info);
                 }else if(tileElement.getText().equals("gate")){
-                    //tilemap[i][count] = new gateTile(new Location(count, i), false);
+                    //tilemap[i][count] = new gateTile(new Location(count, i));
                 }else if(tileElement.getText().equals("exit")){
-                    //tilemap[i][count] = new exitTile(new Location(count, i), true);
-                }
+                    //tilemap[i][count] = new exitTile(new Location(count, i));
+                }**/
                 //Incase an error happens/or missing tile type put a wall tile instead
                 else{
-                    //tilemap[i][count] = new wallTile(new Location(count, i), false);
+                    tilemap[i][count] = new WallTile(new Location(count, i));
                 }
 
                 //count increase to loop through the file
                 count+= 1;
             }
         }
-        return tilemap;
+        Game.instance.setupGame(tilemap, player, new HashMap<Game.KeyColour, Boolean>());
     }
 
     public Player getPlayer(){
         return player;
+    }
+
+    private Game.KeyColour getColour(String colour){
+        if(colour.equals("blue")){
+            return Game.KeyColour.BLUE;
+        }else if(colour.equals("yellow")){
+            return Game.KeyColour.YELLOW;
+        }
+        return null;
     }
 
 }
