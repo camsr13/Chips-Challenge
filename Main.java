e6093a89a33578c4e9124c2e3dafdbc4a6d8bebf
Cameron Richards <richarcame1@ecs.vuw.ac.nz>
2021-09-20 17:42:27 +1200

    Update src/nz/ac/vuw/ecs/swen225/gp21/app/GUIImp.java, Main.java, CustomPanel.java files

---
+++ Main.java
@@ -0,0 +1,11 @@
+package nz.ac.vuw.ecs.swen225.gp21.app;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		GUIImp gui = new GUIImp();
+		gui.getMainWindow().setVisible(true);
+	}
+
+}
--- src/nz/ac/vuw/ecs/swen225/gp21/app/GUIImp.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/app/GUIImp.java
@@ -1,30 +1,47 @@
 package nz.ac.vuw.ecs.swen225.gp21.app;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.util.*;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
-public class GameWindowImp implements GameWindow, Observer {
-     
-     // the game object
-    Game game;
+public class GUIImp implements GUIAbstract{
+
+    // the game object
+    //Game game;
 
     //Swing variable declarations
 
+	//The game frame
 	private final JFrame frame = new JFrame("Game");
 
-    //Create MenuBar and buttons
-	private JMenuBar mb = new JMenuBar();
-	private JMenu menu = new JMenu("Menu");
+	//SideBar to house clock, level and chips count
+	private final JPanel sideBar = new CustomPanel(250, 500);
+	private final JPanel levelPanel = new CustomPanel(250, 100);
+	private final JPanel timePanel = new CustomPanel(250, 100);
+	private final JPanel chipsPanel = new CustomPanel(250, 100);
 
-    public GameWindowImp() {
+    //Create MenuBar and buttons
+	private JMenuBar menuBar = new JMenuBar();
+
+	//Temp board placeholder
+	private final JPanel board = new CustomPanel(500, 500);
+
+	//Content Panel
+	private final JPanel area = new CustomPanel(700, 500);
+
+    public GUIImp() {
 		initGUI();
 	}
 
     private void initGUI() {
-            
-            initFrame();
-            initMenu();
+
+	    initFrame();
+	    initMenu();
+	    initBoard();
+	    initSideBar();
+
     }
 
     private void initFrame() {
@@ -34,7 +51,61 @@ public class GameWindowImp implements GameWindow, Observer {
 		frame.setLocationByPlatform(true);
 		frame.pack();
 	}
-	
+
     private void initMenu(){
-        
+    	//Create menuBar options
+    	JMenu menuGame = new JMenu("Game");
+    	JMenu menuOptions = new JMenu("Options");
+    	JMenu menuLevel = new JMenu("Level");
+    	JMenu menuHelp = new JMenu("Help");
+
+		//Add menu items to menu
+		menuBar.add(menuGame);
+		menuBar.add(menuOptions);
+		menuBar.add(menuLevel);
+		menuBar.add(menuHelp);
+
+
+		//Add menuBar to area
+		frame.setJMenuBar(menuBar);
+
     }
+
+
+    private void initBoard(){
+
+    	//padding around the board
+		area.setLayout(new BorderLayout(3, 3));
+		area.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		board.setBackground(Color.PINK);
+
+	    area.add(board, BorderLayout.CENTER);
+
+    }
+
+    private void initSideBar(){
+
+    	JTextArea levelLabel = new JTextArea("Level");
+    	JTextArea timeLabel = new JTextArea("Time");
+    	JTextArea chipsLabel = new JTextArea("Chips Left");
+
+    	levelPanel.add(levelLabel);
+    	timePanel.add(timeLabel);
+    	chipsPanel.add(chipsLabel);
+
+    	//add parts parts to sideBar
+		sideBar.setBackground(Color.WHITE);
+		sideBar.add(levelPanel, BorderLayout.NORTH);
+		sideBar.add(timePanel, BorderLayout.CENTER);
+		sideBar.add(chipsPanel, BorderLayout.SOUTH);
+
+		//add sidebar to content frame
+    	area.add(sideBar, BorderLayout.LINE_END);
+
+    }
+
+    public JFrame getMainWindow() {
+		return frame;
+	}
+}
