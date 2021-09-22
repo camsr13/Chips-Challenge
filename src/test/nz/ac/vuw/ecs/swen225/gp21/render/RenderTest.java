cca6b0bf8e9c8aaba7eead037d040e6b469423ce
Jac Clarke <clarkejac@ecs.vuw.ac.nz>
2021-09-22 15:28:11 +1200

    Fixed a Bug in boardPanel causing the the x and y axies to be swapped. Updated BoardPanel to load keys and exit images. Now also determine which colour door/key to display. Updated RenderTest to display some keys, doors and exit

--- src/test/nz/ac/vuw/ecs/swen225/gp21/render/RenderTest.java
+++ src/test/nz/ac/vuw/ecs/swen225/gp21/render/RenderTest.java
@@ -2,9 +2,13 @@ package test.nz.ac.vuw.ecs.swen225.gp21.render;
 
 import javax.swing.JFrame;
 
+import nz.ac.vuw.ecs.swen225.gp21.domain.ExitLockTile;
+import nz.ac.vuw.ecs.swen225.gp21.domain.ExitTile;
 import nz.ac.vuw.ecs.swen225.gp21.domain.FreeTile;
 import nz.ac.vuw.ecs.swen225.gp21.domain.Game;
+import nz.ac.vuw.ecs.swen225.gp21.domain.KeyTile;
 import nz.ac.vuw.ecs.swen225.gp21.domain.Location;
+import nz.ac.vuw.ecs.swen225.gp21.domain.LockTile;
 import nz.ac.vuw.ecs.swen225.gp21.domain.Player;
 import nz.ac.vuw.ecs.swen225.gp21.domain.Tile;
 import nz.ac.vuw.ecs.swen225.gp21.domain.WallTile;
@@ -19,10 +23,10 @@ public class RenderTest {
 			{new WallTile(null),new WallTile(null), new WallTile(null), new WallTile(null), new WallTile(null), new WallTile(null), new WallTile(null), new WallTile(null), new WallTile(null)},
 			{new WallTile(null),new WallTile(null), new WallTile(null), new WallTile(null), new WallTile(null), new WallTile(null), new WallTile(null), new WallTile(null), new WallTile(null)},
 			{new WallTile(null),new WallTile(null), new FreeTile(null), new FreeTile(null), new FreeTile(null), new FreeTile(null), new FreeTile(null), new WallTile(null), new WallTile(null)},
-			{new WallTile(null),new WallTile(null), new FreeTile(null), new FreeTile(null), new FreeTile(null), new FreeTile(null), new FreeTile(null), new WallTile(null), new WallTile(null)},
-			{new WallTile(null),new WallTile(null), new FreeTile(new Location(2, 4)), new FreeTile(null), new FreeTile(null), new FreeTile(null), new FreeTile(null), new WallTile(null), new WallTile(null)},
-			{new WallTile(null),new WallTile(null), new FreeTile(null), new FreeTile(null), new FreeTile(null), new FreeTile(null), new FreeTile(null), new WallTile(null), new WallTile(null)},
-			{new WallTile(null),new WallTile(null), new FreeTile(null), new FreeTile(null), new FreeTile(null), new FreeTile(null), new FreeTile(null), new WallTile(null), new WallTile(null)},
+			{new WallTile(null),new WallTile(null), new FreeTile(null), new FreeTile(null),  new KeyTile(null, Game.KeyColour.BLUE), new FreeTile(null), new FreeTile(null), new WallTile(null), new WallTile(null)},
+			{new WallTile(null),new LockTile(null, Game.KeyColour.YELLOW), new FreeTile(null), new KeyTile(null, Game.KeyColour.BLUE), new FreeTile(null), new FreeTile(null), new FreeTile(null), new WallTile(null), new WallTile(null)},
+			{new WallTile(null),new LockTile(null, Game.KeyColour.BLUE), new FreeTile(null), new FreeTile(null), new FreeTile(null), new FreeTile(null), new FreeTile(null), new WallTile(null), new WallTile(null)},
+			{new WallTile(null),new WallTile(null), new FreeTile(null), new ExitLockTile(null), new ExitTile(null), new FreeTile(null), new FreeTile(null), new WallTile(null), new WallTile(null)},
 			{new WallTile(null),new WallTile(null), new WallTile(null), new WallTile(null), new WallTile(null), new WallTile(null), new WallTile(null), new WallTile(null), new WallTile(null)},
 			{new WallTile(null),new WallTile(null), new WallTile(null), new WallTile(null), new WallTile(null), new WallTile(null), new WallTile(null), new WallTile(null), new WallTile(null)}
 	};
