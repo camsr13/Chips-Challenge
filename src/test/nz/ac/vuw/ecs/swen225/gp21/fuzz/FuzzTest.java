5449a8374ac959d3c24ebe47f7a97bfaa353e5e9
Rhys Macdonald <macdonrhys@ecs.vuw.ac.nz>
2021-09-23 11:13:10 +1200

    Added documentation to FuzzTest.java

--- src/test/nz/ac/vuw/ecs/swen225/gp21/fuzz/FuzzTest.java
+++ src/test/nz/ac/vuw/ecs/swen225/gp21/fuzz/FuzzTest.java
@@ -1,9 +1,8 @@
 package test.nz.ac.vuw.ecs.swen225.gp21.fuzz;
 
-import nz.ac.vuw.ecs.swen225.gp21.app.GUIImp;
+//import nz.ac.vuw.ecs.swen225.gp21.app.GUIImp;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.awt.event.KeyAdapter;
 import java.awt.event.MouseAdapter;
 import java.util.*;
@@ -12,11 +11,12 @@ import java.util.concurrent.*;
 import static org.mockito.Mockito.mock;
 
 /**
+ * Contains a test for each of the game's two levels.
  * @author Rhys Macdonald - 300516792
  */
 public class FuzzTest {
     private enum Direction {
-        NORTH, SOUTH, EAST, WEST;
+        NORTH, SOUTH, EAST, WEST
     }
 
     // Placeholders
@@ -25,14 +25,23 @@ public class FuzzTest {
     private final MouseAdapter south = mock(MouseAdapter.class);
     private final MouseAdapter west = mock(MouseAdapter.class);
     private final KeyAdapter reset = mock(KeyAdapter.class);
-    private final List<MouseAdapter> directions = Arrays.asList(north, east, south, west);
+//    private final List<MouseAdapter> directions = Arrays.asList(north, east, south, west);
 
     private final int GRID_WIDTH = 21;
     private final int[] grid = new int[GRID_WIDTH * GRID_WIDTH];
 
-    private final int MOVES = 100;
+
+//    private final int MOVES = 100;
+    /**
+     * The number of seconds the random exploration should run (at the most).
+     */
     private final int TIMEOUT = 30;
 
+    /**
+     * Converts a specified direction to the MouseAdapter used to move in that direction.
+     * @param d The direction.
+     * @return The MouseAdapter.
+     */
     private MouseAdapter getAdapterFromDirection(Direction d) {
         switch(d) {
             case NORTH: return north;
@@ -43,6 +52,10 @@ public class FuzzTest {
         }
     }
 
+    /**
+     * Converts the grid into a human-readable string with the visit numbers of each cell.
+     * @return The string.
+     */
     private String gridToString() {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < grid.length; i++) {
@@ -66,6 +79,12 @@ public class FuzzTest {
         setGridAt(row, col, getGridAt(row, col) + 1);
     }
 
+    /**
+     * Get a specified cell's neighbours, and store them in a map with their corresponding visit numbers.
+     * @param row The row used to specify the cell.
+     * @param col The column used to specify the cell.
+     * @return The map.
+     */
     private Map<Direction, Integer> getNeighbours(int row, int col) {
         Map<Direction, Integer> r = new HashMap<>();
         r.put(Direction.NORTH, getGridAt(row-1, col));
@@ -75,6 +94,13 @@ public class FuzzTest {
         return r;
     }
 
+    /**
+     * Finds a specified cell's neighbouring cells with the least visits, converts those into directions and stores the
+     * directions in a list.
+     * @param row The row used to specify the cell.
+     * @param col The column used to specify the cell.
+     * @return The list of directions.
+     */
     private List<Direction> getPreferredDirections(int row, int col) {
         Map<Direction, Integer> neighbours = getNeighbours(row, col);
         List<Direction> preferredDirections = new ArrayList<>();
@@ -94,22 +120,28 @@ public class FuzzTest {
         return preferredDirections;
     }
 
-    private void performRandomMoves() {
-        Random random = new Random();
-        for (int i = 0; i < MOVES; i++) {
-            MouseAdapter direction = directions.get(random.nextInt(directions.size()));
-            direction.mouseClicked(null);
-        }
-    }
+//    private void performRandomMoves() {
+//        Random random = new Random();
+//        for (int i = 0; i < MOVES; i++) {
+//            MouseAdapter direction = directions.get(random.nextInt(directions.size()));
+//            direction.mouseClicked(null);
+//        }
+//    }
 
+    /**
+     * Explores a set number of random paths from the start, resetting the level after each path.
+     */
     private void exploreGrid() {
         for (int i = 0; i < 200; i++) {
             explorePath();
             reset.keyPressed(null);
         }
-        System.out.println(gridToString());
+//        System.out.println(gridToString());
     }
 
+    /**
+     * Explores a random path, noting on the grid where it has already been.
+     */
     private void explorePath() {
         Random random = new Random();
 
@@ -147,15 +179,17 @@ public class FuzzTest {
     }
 
     /**
-     * Tests level 1.
+     * Tests level 1. Starts by traversing random paths from the start, then purposely tries to break the level using
+     * hardcoded paths.
      */
     @Test
     void test1() {
         // Load in level 1
-        GUIImp gui = new GUIImp();
-        gui.getMainWindow().setVisible(true);
+//        GUIImp gui = new GUIImp();
+//        gui.getMainWindow().setVisible(true);
 
 
+        // This code block runs exploreGrid() until either it finishes, or a certain time limit is reached.
         final ExecutorService es = Executors.newSingleThreadExecutor();
         Future<?> future = es.submit(this::exploreGrid);
         try {
@@ -168,7 +202,8 @@ public class FuzzTest {
     }
 
     /**
-     * Tests level 2.
+     * Tests level 2. Starts by traversing random paths from the start, then purposely tries to break the level using
+     * hardcoded paths.
      */
     @Test
     void test2() {
