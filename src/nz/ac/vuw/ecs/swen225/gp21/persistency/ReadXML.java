2cdbb1ba573bd22ac04b3ebf779d35ff455792a2
Rhys Hanrahan <rhys.c.hanrahan@gmail.com>
2021-09-29 21:57:06 +1300

    Added time read/write, and within level files Added getTimeLimit() in ReadXML which can be called by GUIImp after ReadXMLFile to get the time for the level Added updateTime(int time) which GUIImp will have to call when their local timeRemaining is updated, so timeRemaining can be saved to currentSaves Used spotBug to fix all found bugs Used Checkstyle to fix found errors/improvements

--- src/nz/ac/vuw/ecs/swen225/gp21/persistency/ReadXML.java
+++ src/nz/ac/vuw/ecs/swen225/gp21/persistency/ReadXML.java
@@ -1,7 +1,6 @@
 package nz.ac.vuw.ecs.swen225.gp21.persistency;
 
 import nz.ac.vuw.ecs.swen225.gp21.domain.*;
-import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
@@ -22,72 +21,70 @@ import java.util.List;
  *
  */
 
-public class readXML {
+public class ReadXML {
 
-    //Test map size
-    private Tile[][] tilemap;
-    private Player player;
+    //Set default timeLimit to 999 seconds
+    private int timeLimit = 999;
 
 
     /**
      *
-     * incase ReadXMLFile is called without a filename it will call the real ReadXMLFile with the testmap as the filename
+     * in case ReadXMLFile is called without a filename it will call the real ReadXMLFile with the testMap as the filename
      *
-     * @throws JDOMException
-     * @throws IOException
      */
     public void readXMLFile() throws JDOMException, IOException {
-        readXMLFile("src/nz/ac/vuw/ecs/swen225/gp21/persistency/testMap.xml");
+        readXMLFile("levels/testMap.xml");
     }
 
     /**
      *
      * readXMLFile takes a fileName as a string input, then reads that file to create a tilemap and player object
      *
-     * @throws JDOMException
-     * @throws IOException
      */
     public void readXMLFile(String fileDirectory) throws JDOMException, IOException {
         //Variables to hold the position where the file is up to
-        Element parentElement = null;
-        Element childElement = null;
-        Element map = ((Document) (new SAXBuilder()).build(new File(fileDirectory))).getRootElement();
+        Element parentElement;
+        Element childElement;
+        Element map = (new SAXBuilder()).build(new File(fileDirectory)).getRootElement();
 
         //Get map variables
         parentElement = map.getChild("mapVariables");
         //get the map size and create a new tileMap (2d array) of that size
-        tilemap = new Tile[Integer.parseInt(parentElement.getChild("sizeX").getText())][Integer.parseInt(parentElement.getChild("sizeY").getText())];
+        //Test map size
+        Tile[][] tilemap = new Tile[Integer.parseInt(parentElement.getChild("sizeX").getText())][Integer.parseInt(parentElement.getChild("sizeY").getText())];
         //get and store the totalTreasures and collectedTreasures
         int totalTreasure = Integer.parseInt(parentElement.getChild("total").getText());
         int collectedTreasures = Integer.parseInt(parentElement.getChild("collected").getText());
         //get the coordinates of the exit lock
         int exitLockX = Integer.parseInt(parentElement.getChild("exitLockX").getText());
         int exitLockY = Integer.parseInt(parentElement.getChild("exitLockY").getText());
+        //get the time limit for the level
+        timeLimit = Integer.parseInt(parentElement.getChild("time").getText());
 
         //Get keysHeld
-        HashMap<Game.KeyColour, Integer> keysHeld = new HashMap<Game.KeyColour, Integer>();
+        HashMap<Game.KeyColour, Integer> keysHeld = new HashMap<>();
         parentElement = map.getChild("keysHeld");
         Iterator<Element> Iterator = parentElement.getChildren("key").iterator();
         //For every key colour, add the it to the hashmap
         while(Iterator.hasNext()) {
-            childElement = (Element) Iterator.next();
+            childElement = Iterator.next();
             keysHeld.put(getColour(childElement.getAttributeValue("colour")),Integer.parseInt(childElement.getText()));
         }
 
         //Get player info (location)
         parentElement = map.getChild("player");
         Iterator = parentElement.getChildren("location").iterator();
-        //There will always be two location elements in the XML file for the players x and y coords
-        player = new Player(new Location(Integer.parseInt(Iterator.next().getText()), Integer.parseInt(Iterator.next().getText())), Integer.parseInt(parentElement.getChild("frozen").getText()));
+        //There will always be two location elements in the XML file for the players x and y coordinates
+        Player player = new Player(new Location(Integer.parseInt(Iterator.next().getText()), Integer.parseInt(Iterator.next().getText())), Integer.parseInt(parentElement.getChild("frozen").getText()));
 
         //get actors
-        List<Actor> actors = new ArrayList<Actor>();
+        List<Actor> actors = new ArrayList<>();
         parentElement = map.getChild("actors");
         //check if there are actors on the current xml level
         if(parentElement != null){
             Iterator = parentElement.getChildren("freeze").iterator();
             while(Iterator.hasNext()){
-                childElement = (Element) Iterator.next();
+                childElement = Iterator.next();
                 actors.add(new FreezeActor(new Location(Integer.parseInt(childElement.getAttributeValue("coordX")), Integer.parseInt(childElement.getAttributeValue("coordY"))), getDirection(childElement.getText())));
             }
         }
@@ -96,39 +93,27 @@ public class readXML {
         //Loop through the file and goes through all the tileRows
         List<Element> rowsList = map.getChildren("tileRow");
         for(int i = 0; i < rowsList.size(); i++){
-            parentElement = (Element) rowsList.get(i);
+            parentElement = rowsList.get(i);
             Iterator = parentElement.getChildren("tile").iterator();
 
             //Loops through all the tiles within each tileRow
             int count = 0;
             while(Iterator.hasNext()){
-                childElement = (Element) Iterator.next();
+                childElement = Iterator.next();
 
                 //check for what the tile type is then add it to the tile map
-                if(childElement.getText().equals("free")){
-                    tilemap[count][i] = new FreeTile(new Location(count, i));
-                }else if(childElement.getText().equals("wall")){
-                    tilemap[count][i] = new WallTile(new Location(count, i));
-                }else if(childElement.getText().equals("door")){
-                    tilemap[count][i] = new LockTile(new Location(count, i), getColour(childElement.getAttributeValue("colour")));
-                }else if(childElement.getText().equals("key")){
-                    tilemap[count][i] = new KeyTile(new Location(count, i), getColour(childElement.getAttributeValue("colour")));
-                }else if(childElement.getText().equals("treasure")){
-                    tilemap[count][i] = new TreasureTile(new Location(count, i));
-                }else if(childElement.getText().equals("info")){
-                    tilemap[count][i] = new InfoTile(new Location(count, i), childElement.getAttributeValue("info"));
-                }else if(childElement.getText().equals("gate")){
-                    tilemap[count][i] = new ExitLockTile(new Location(count, i));
-                }else if(childElement.getText().equals("exit")){
-                    tilemap[count][i] = new ExitTile(new Location(count, i));
-                }else if(childElement.getText().equals("arrow")){
-                    tilemap[count][i] = new ArrowTile(new Location(count, i), getDirection(childElement.getAttributeValue("direction")));
-                }else if(childElement.getText().equals("freeze")){
-                    tilemap[count][i] = new FreezeTile(new Location(count, i));
-                }
-                //Incase an error happens/or missing tile type put a wall tile instead
-                else{
-                    tilemap[count][i] = new WallTile(new Location(count, i));
+                //In case an error happens/or missing tile type put a wall tile instead
+                switch (childElement.getText()) {
+                    case "free" -> tilemap[count][i] = new FreeTile(new Location(count, i));
+                    case "door" -> tilemap[count][i] = new LockTile(new Location(count, i), getColour(childElement.getAttributeValue("colour")));
+                    case "key" -> tilemap[count][i] = new KeyTile(new Location(count, i), getColour(childElement.getAttributeValue("colour")));
+                    case "treasure" -> tilemap[count][i] = new TreasureTile(new Location(count, i));
+                    case "info" -> tilemap[count][i] = new InfoTile(new Location(count, i), childElement.getAttributeValue("info"));
+                    case "gate" -> tilemap[count][i] = new ExitLockTile(new Location(count, i));
+                    case "exit" -> tilemap[count][i] = new ExitTile(new Location(count, i));
+                    case "arrow" -> tilemap[count][i] = new ArrowTile(new Location(count, i), getDirection(childElement.getAttributeValue("direction")));
+                    case "freeze" -> tilemap[count][i] = new FreezeTile(new Location(count, i));
+                    default -> tilemap[count][i] = new WallTile(new Location(count, i));
                 }
 
                 //count increase to loop through the file
@@ -143,20 +128,16 @@ public class readXML {
      * getColour is a method to get a string colour and return the corresponding enum colour from Game
      * returns null at the end of the method, which should never be reached if getColour checks for all valid colours
      *
-     * @param colour
      * @return colour enum
      */
     private Game.KeyColour getColour(String colour){
-        if(colour.equals("blue")){
-            return Game.KeyColour.BLUE;
-        }else if(colour.equals("yellow")){
-            return Game.KeyColour.YELLOW;
-        }else if(colour.equals("red")){
-            return Game.KeyColour.RED;
-        }else if(colour.equals("green")){
-            return Game.KeyColour.GREEN;
-        }
-        return null;
+        return switch (colour) {
+            case "blue" -> Game.KeyColour.BLUE;
+            case "yellow" -> Game.KeyColour.YELLOW;
+            case "red" -> Game.KeyColour.RED;
+            case "green" -> Game.KeyColour.GREEN;
+            default -> null;
+        };
     }
 
     /**
@@ -164,25 +145,30 @@ public class readXML {
      * getDirection is a method used when creating tiles for the tileMap
      * When creating an arrowTile a string direction from the xml file is passed and the enum Game.direction is returned
      *
-     * @param direction
      * @return enum direction from game
      */
     private Game.Direction getDirection(String direction){
-        if(direction.equals("up")){
-            return Game.Direction.UP;
-        }else if(direction.equals("down")){
-            return Game.Direction.DOWN;
-        }else if(direction.equals("left")){
-            return Game.Direction.LEFT;
-        }else if(direction.equals("right")){
-            return Game.Direction.RIGHT;
-        }
-        return null;
+        return switch (direction) {
+            case "up" -> Game.Direction.UP;
+            case "down" -> Game.Direction.DOWN;
+            case "left" -> Game.Direction.LEFT;
+            case "right" -> Game.Direction.RIGHT;
+            default -> null;
+        };
+    }
+
+
+    /**
+     *
+     * @return the time limit within the level file
+     */
+    public int getTimeLimit(){
+        return timeLimit;
     }
 
     /**
      *
-     * fetches the current game instance from Game for Recorder
+     * fetches the current game instance from Game
      * @return Game.instance
      */
     public Game getGameInstance(){
--- src/test/nz/ac/vuw/ecs/swen225/gp21/persistency/WriteFileTests.java
+++ src/test/nz/ac/vuw/ecs/swen225/gp21/persistency/WriteFileTests.java
@@ -1,8 +1,8 @@
 package test.nz.ac.vuw.ecs.swen225.gp21.persistency;
 
 import nz.ac.vuw.ecs.swen225.gp21.domain.Game;
-import nz.ac.vuw.ecs.swen225.gp21.persistency.readXML;
-import nz.ac.vuw.ecs.swen225.gp21.persistency.writeXML;
+import nz.ac.vuw.ecs.swen225.gp21.persistency.ReadXML;
+import nz.ac.vuw.ecs.swen225.gp21.persistency.WriteXML;
 import org.jdom2.JDOMException;
 import org.junit.jupiter.api.Test;
 
@@ -12,13 +12,14 @@ import java.io.IOException;
 public class WriteFileTests {
     @Test
     public void testWriteCurrentSave_01() throws JDOMException, IOException {
+        //needed for the test, else game is null and read/write xml does not work
         Game game = new Game();
-        readXML readXML = new readXML();
-        readXML.readXMLFile("src/nz/ac/vuw/ecs/swen225/gp21/persistency/level2.xml");
-        writeXML instance = new writeXML();
-        instance.writeXMLFile(instance.generateDocument(), "src/nz/ac/vuw/ecs/swen225/gp21/persistency/currentSave.xml");
+        ReadXML readXML = new ReadXML();
+        readXML.readXMLFile("levels/level2.xml");
+        WriteXML instance = new WriteXML();
+        instance.writeXMLFile(instance.generateDocument(), "levels/currentSave.xml");
 
-        assert(new File("src/nz/ac/vuw/ecs/swen225/gp21/persistency/currentSave.xml").isFile());
+        assert(new File("levels/currentSave.xml").isFile());
     }
 
 }
