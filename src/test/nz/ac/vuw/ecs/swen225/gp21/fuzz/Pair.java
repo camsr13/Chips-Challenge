a0f336fb6020d70212ecbfcfd4724aed5abf4705
Rhys Macdonald <macdonrhys@ecs.vuw.ac.nz>
2021-10-01 18:46:59 +1300

    Changed algorithm so fuzz doesn't open doors until fully explored previous rooms

---
+++ src/test/nz/ac/vuw/ecs/swen225/gp21/fuzz/Pair.java
@@ -0,0 +1,26 @@
+package test.nz.ac.vuw.ecs.swen225.gp21.fuzz;
+
+import java.util.Objects;
+
+public class Pair {
+  public final int row;
+  public final int col;
+
+  public Pair(int row, int col) {
+    this.row = row;
+    this.col = col;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Pair pair = (Pair) o;
+    return row == pair.row && col == pair.col;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(row, col);
+  }
+}
